e460b7ff1c37fe483902727d7d8f853b92f3e2a8
Alexandr Panteleev <kibal4iw@gmail.com>
2015-08-28 20:28:36 +0300

    Добавлена функция для извлечения данных

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+public static function get_сid() {
+	list($version, $domainDepth, $cid1, $cid2) = preg_split('[\.]', $_COOKIE["_ga"], 4);
+	$ga = array('version' => $version, 'domainDepth' => $domainDepth, 'cid' => $cid1.'.'.$cid2);
+
+	return $ga;
+}
+
+echo '<pre>';
+print_r(get_сid());
+echo '</pre>';
+
+?>
